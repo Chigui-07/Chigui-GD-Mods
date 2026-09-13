459d365b40d9b95f7e57eae5454374d34391dc30
Chigui <denischigui16@gmail.com>
2026-09-13 14:26:01 -0600

    feat: add initial Demon Tracker menu hook

---
+++ src/main.cpp
@@ -0,0 +1,36 @@
+#include <Geode/Geode.hpp>
+#include <Geode/modify/MenuLayer.hpp>
+
+using namespace geode::prelude;
+
+class $modify(DemonTrackerMenuLayer, MenuLayer) {
+    bool init() {
+        if (!MenuLayer::init()) {
+            return false;
+        }
+
+        log::info("Demon Tracker v0.1.0 loaded successfully.");
+
+        auto button = CCMenuItemSpriteExtra::create(
+            CCSprite::createWithSpriteFrameName("GJ_likeBtn_001.png"),
+            this,
+            menu_selector(DemonTrackerMenuLayer::onDemonTracker)
+        );
+
+        if (auto menu = this->getChildByID("bottom-menu")) {
+            button->setID("demon-tracker-button"_spr);
+            menu->addChild(button);
+            menu->updateLayout();
+        }
+
+        return true;
+    }
+
+    void onDemonTracker(CCObject*) {
+        FLAlertLayer::create(
+            "Demon Tracker",
+            "<cg>Demon Tracker v0.1.0</c> esta cargado correctamente.\n\nSiguiente paso: construir la interfaz de listas y el sistema de clasificacion.",
+            "OK"
+        )->show();
+    }
+};
