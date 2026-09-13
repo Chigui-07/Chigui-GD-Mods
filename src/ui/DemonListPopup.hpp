a8c09bd6fdb9af13a5b16e0e14e34bdbc1499403
Chigui <denischigui16@gmail.com>
2026-09-13 17:43:23 -0600

    feat: make demon list entries selectable

--- src/ui/DemonListPopup.hpp
+++ src/ui/DemonListPopup.hpp
@@ -9,9 +9,16 @@ using namespace geode::prelude;
 class DemonListPopup : public Popup {
 protected:
     DemonCategory m_category = DemonCategory::All;
+    std::vector<DemonEntry const*> m_demons;
 
     bool init(DemonCategory category);
-    CCNode* createDemonRow(int rank, DemonEntry const& demon, float width);
+    CCNode* createDemonRow(
+        int rank,
+        int index,
+        DemonEntry const& demon,
+        float width
+    );
+    void onDemonSelected(CCObject* sender);
 
 public:
     static DemonListPopup* create(DemonCategory category);
